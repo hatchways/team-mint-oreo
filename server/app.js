a9483fab1f00ec02c20966dcff7797490f3aef03
BQ <qian.brian@gmail.com>
2020-02-09 11:59:32 -0500

    fixed nodemon error (hopefully)

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-const createError = require("http-errors");
-const express = require("express");
-const { join } = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require('http-errors');
+const express = require('express');
+const { join } = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const indexRouter = require("./routes/index");
-const pingRouter = require("./routes/ping");
+const indexRouter = require('./routes/index');
+const pingRouter = require('./routes/ping');
 
 // DB connection
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
 
 const { json, urlencoded } = express;
 
@@ -17,14 +17,14 @@ var app = express();
 // Start the DB
 connectDB();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/ping", pingRouter);
+app.use('/', indexRouter);
+app.use('/ping', pingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
