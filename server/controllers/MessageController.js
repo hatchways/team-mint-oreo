ed4939aa193136a77e631bf2e3aa2fe5d8c00ef8
BQ <qian.brian@gmail.com>
2020-02-10 17:51:20 -0500

    message controller started

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,2 +1,28 @@
-const mongoose = require('mongoose');
-const messageSchema = require('../Models/User');
+const Message = require('../models/Message');
+
+const createMessage = async msg => {
+  const newMessage = new Message(msg);
+  try {
+    const savedMsg = await newMessage.save();
+    console.log('creating message', savedMsg);
+    return savedMsg;
+  } catch (error) {
+    return { error };
+  }
+};
+
+const getMessagesByChatId = async (chatId, limit = 50, skip = 0) => {
+  try {
+    const messages = await Message.find({ chatId }, null, skip)
+      .limit(limit)
+      .sort('-createdAt');
+
+    return messages;
+  } catch (error) {
+    return { error };
+  }
+};
+module.exports = {
+  createMessage,
+  getMessagesByChatId,
+};
