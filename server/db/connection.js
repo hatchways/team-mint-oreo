331a6941a6b147a0a23fdb817f53ff97fbcca64f
Sang min Lee <sang.m.lee@mail.mcgill.ca>
2020-02-10 13:32:31 -0500

    Created User model & added user controller function

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 // require("dotenv").config();
 
 // Connect to Mongo
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-        console.log(`MongoDB connected at ${conn.connection.host}`);
-    } catch(err) {
-        console.error(err);
-        process.exit(1);    // Exit out of the app with a failure
-    }
+    console.log(`MongoDB connected at ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // Exit out of the app with a failure
+  }
 };
 
 module.exports = connectDB;
