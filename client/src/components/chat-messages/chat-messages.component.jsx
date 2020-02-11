74fae38047e7523d27d994541da5a442f982356d
Rocket-Fish <rocket.fish@mail.com>
2020-02-11 10:51:38 -0500

    chat messages now scroll when overflowing

--- client/src/components/chat-messages/chat-messages.component.jsx
+++ client/src/components/chat-messages/chat-messages.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
 const ChatMessages = ({ heights = [300] }) => {
   return (
-    <Box minHeight={combineHeight(heights)} bgcolor="grey.200">
-      <Box>text</Box>
+    <Box
+      minHeight={combineHeight(heights)}
+      maxHeight={combineHeight(heights)}
+      style={{ overflow: 'auto' }}
+    >
+      <Box>
+        {list.map(item => (
+          <Typography key={item}>text</Typography>
+        ))}
+      </Box>
     </Box>
   );
 };
@@ -17,3 +25,37 @@ const combineHeight = heights =>
       currentElement !== null && accumulator + Math.round(currentElement),
     0
   );
+
+const list = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+];
