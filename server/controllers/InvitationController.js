819868747797eaa4cb4438c1cddfad9838780a4b
BQ <qian.brian@gmail.com>
2020-02-10 19:52:25 -0500

    invitation controller created

--- server/controllers/InvitationController.js
+++ server/controllers/InvitationController.js
@@ -9,20 +9,32 @@ const createInvitation = async (fromUser, toUser) => {
   try {
     // Invitation is successfully created
     const savedInvitation = await invitation.save();
-    console.log('New invitation is created');
+    console.log('New invitation is created', savedInvitation);
   } catch (err) {
     // Failed to create an invitation
     console.error(err);
   }
 };
 
-const addPendingInvitation = async invitation => {
-  /* 
-        probably the method that adds pending invitation to 
-        the user that received one
-  */
+const getPendingInvitations = async email => {
+  try {
+    const incoming = await Invitation.find({ toUser: { $eq: { email } } });
+    console.log(incoming);
+  } catch (err) {
+    console.error('error getting invites', err);
+  }
+};
+
+const deleteInvitation = (fromUser, toUser) => {
+  try {
+    Invitation.findOneAndDelete({ fromUser, toUser });
+  } catch (err) {
+    console.error('del. inv fail', err);
+  }
 };
 
 module.exports = {
   createInvitation,
+  getPendingInvitations,
+  deleteInvitation,
 };
