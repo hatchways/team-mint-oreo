d9ff2ec6614f85807f3973a1a6c7e671b6fc8356
Rocket-Fish <rocket.fish@mail.com>
2020-02-12 17:42:48 -0500

    completed invite friends UI

--- client/src/components/invite-friend-backdrop/invite-friend-backdrop.component.jsx
+++ client/src/components/invite-friend-backdrop/invite-friend-backdrop.component.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Box, Grid, Card, Backdrop, ClickAwayListener, TextField } from '@material-ui/core';
+import { Button, Box, Grid, Card, Backdrop, ClickAwayListener } from '@material-ui/core';
 
 import { store as directoryStore } from '../../store/directory/directory.provider';
 import DirectoryActionTypes from '../../store/directory/directory.types';
 
 import EmailField from '../email-field/email-field.component';
 import CopyField from '../copy-field/copy-field.component';
+
+import { IconButton } from '@material-ui/core';
+import Clear from '@material-ui/icons/Clear';
 
 const useStyles = makeStyles(theme => ({
   backdrop: {
@@ -66,6 +69,11 @@ const InviteFriendBackdrop = () => {
     setEmailFields(newList);
   };
 
+  const composeEmails = () => {
+    const emailList = emailFields.map(field => field.value);
+    console.log(emailList);
+  };
+
   useEffect(() => {
     console.log(emailFields);
   }, [emailFields]);
@@ -76,27 +84,42 @@ const InviteFriendBackdrop = () => {
       {showBackdrop && (
         <ClickAwayListener onClickAway={handleClose}>
           <Card>
-            <Box m={10}>
-              <Grid container direction="column" justify="center" alignItems="stretch">
+            <Box marginRight={3} marginLeft={10} marginBottom={10} marginTop={3}>
+              <Grid container direction="column" justify="center" alignItems="flex-end">
                 <Grid item>
-                  <h1>Invite friends to messenger</h1>
-                </Grid>
-                <Grid item>
-                  <h3> Send your friends an email</h3>
-                  {emailFields.map(field => (
-                    <EmailField
-                      key={field.id}
-                      {...field}
-                      onAddClick={handleAddFriendField}
-                      onChange={recordFieldChanges}
-                    />
-                  ))}
-                </Grid>
-                <Grid item>
-                  <h3> Or share referral link:</h3>
-                  <CopyField placeholder={'http://localhost:3000/dashboard'} />
+                  <IconButton padding={10} onClick={handleClose}>
+                    <Clear />
+                  </IconButton>
                 </Grid>
               </Grid>
+              <Box marginRight={7}>
+                <Grid container direction="column" justify="center" alignItems="center">
+                  <Grid item>
+                    <h1>Invite friends to messenger</h1>
+                  </Grid>
+                  <Grid item>
+                    <h3> Send your friends an email</h3>
+                    {emailFields.map(field => (
+                      <EmailField
+                        key={field.id}
+                        {...field}
+                        onAddClick={handleAddFriendField}
+                        onChange={recordFieldChanges}
+                      />
+                    ))}
+                  </Grid>
+                  <Grid item>
+                    <h3> Or share referral link:</h3>
+                    <CopyField placeholder={'http://localhost:3000/dashboard'} />
+                  </Grid>
+                  <Grid item>
+                    <h3></h3>
+                    <Button variant="contained" color="primary" onClick={composeEmails}>
+                      Send Invite
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
             </Box>
           </Card>
         </ClickAwayListener>
