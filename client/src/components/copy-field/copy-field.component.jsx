040cae81d28bcb9a2205057b3cdf54b08f5ae058
Rocket-Fish <rocket.fish@mail.com>
2020-02-12 17:14:45 -0500

    added a library to complete copy to clipboard feature

--- client/src/components/copy-field/copy-field.component.jsx
+++ client/src/components/copy-field/copy-field.component.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, InputBase, IconButton, SvgIcon } from '@material-ui/core';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,6 +25,7 @@ const useStyles = makeStyles(theme => ({
 
 const CopyField = ({ placeholder }) => {
   const classes = useStyles();
+  const textAreaRef = useRef(null);
 
   const handleChange = e => {
     e.preventDefault();
@@ -42,13 +44,16 @@ const CopyField = ({ placeholder }) => {
         className={classes.input}
         value={placeholder}
         inputProps={{ 'aria-label': 'search listing' }}
+        ref={textAreaRef}
         disabled
       />
-      <IconButton type="submit" className={classes.iconButton} aria-label="copy">
-        <SvgIcon>
-          <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />{' '}
-        </SvgIcon>
-      </IconButton>
+      <CopyToClipboard text={placeholder}>
+        <IconButton type="submit" className={classes.iconButton} aria-label="copy">
+          <SvgIcon>
+            <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+          </SvgIcon>
+        </IconButton>
+      </CopyToClipboard>
     </Box>
   );
 };
