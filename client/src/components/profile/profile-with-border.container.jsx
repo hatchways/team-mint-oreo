9b63229fc1847698d3fc00bfce326a0208ab6b42
Rocket-Fish <rocket.fish@mail.com>
2020-02-12 20:35:26 -0500

    rewmoved with=border to remove file bloat

--- client/src/components/profile/profile-with-border.container.jsx
+++ client/src/components/profile/profile-with-border.container.jsx
@@ -1,6 +1,23 @@
-import WithBorder from '../with-border/with-border.component';
+import React from 'react';
+import { Paper, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
 import Profile from './profile.component';
 
-const ProfileWithBorder = WithBorder(Profile, props => props);
+const useStyles = makeStyles(theme => ({
+  componentStyle: {
+    borderRadius: 16,
+    backgroundColor: 'white',
+  },
+}));
+
+const ProfileWithBorder = ({ ...props }) => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.componentStyle}>
+      <Profile {...props} />
+    </Box>
+  );
+};
 
 export default ProfileWithBorder;
