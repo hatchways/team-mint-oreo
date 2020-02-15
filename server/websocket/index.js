f8d7af7f6bf69ceb7e42ea3a9f1b8a3bb0f970f7
BQ <qian.brian@gmail.com>
2020-02-15 07:10:13 -0500

    separated websocket into its own files

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,82 +1,19 @@
 const socketio = require('socket.io');
 const db = require('../controllers');
-const { cache } = require('../utils/Cache');
+const onLogin = require('./login');
+const onSend = require('./sendMsg');
 
 /* SOCKET METHODS */
 
 // On Login---------------------------------------------
-const registerSocketId = (socket, userId) => {
-  db.user.setSocketIdById(userId, socket.id);
-};
 
 const addChatroom = async (socket, userId, chatId) => {
   await db.user.addChatById(userId, chatId);
   await db.chatroom.addUser(userId, chatId);
 };
 
-const joinChatrooms = async (socket, userId) => {
-  const chatroomList = await db.user.getChatsById(userId);
-
-  // No chatrooms available
-  if (!chatroomList || chatroomList.length <= 0) {
-    console.log('No chatrooms available yet');
-    return;
-  }
-
-  chatroomList.forEach(room => socket.join(room));
-};
-
 const acceptInvitation = async (socket, userId, friendId) => {
   await db.user.addFriend(userId, friendId);
-};
-
-const notifyFriends = async (socket, userId) => {
-  const friendSocketList = await db.user.getFriendsSocketsById(userId);
-  if (!friendSocketList || !friendSocketList.length) {
-    console.log('No friends are online');
-    return;
-  }
-  friendSocketList.forEach(friend => socket.to(friend).emit('userOnline', userId));
-};
-
-// On send message ------------------------------------------------------------
-const getLanguageAndIdList = async chatId => {
-  let list = cache.get(chatId);
-  if (!list) list = await db.chatroom.getLanguagesAndIds(chatId);
-  cache.set(chatId, list);
-  return list;
-};
-
-const translateMessage = async msgObject => {
-  // ⚠️ check cache for active chat languages
-  const { originalText, chatId } = msgObject;
-  const idAndLanguageList = await getLanguageAndIdList(chatId);
-
-  // remove sender from translation list
-  idAndLanguageList.filter(pair => pair.id !== chatId);
-  const translationAPI = { translate: (x, y) => x };
-  const translatedText = await Promise.all(
-    idAndLanguageList.map(({ language }) => translationAPI.translate(originalText, language))
-  );
-
-  // const translationAndIds = idAndLanguageList.map(({ id }, i) => ({ id, text: translatedText[i] }));
-  const idTranslationMap = idAndLanguageList.reduce(
-    (a, b, i) => ({ ...a, [b.id]: translatedText[i] }),
-    {}
-  );
-
-  // ⚠️ cache translation
-  return idTranslationMap;
-};
-
-const sendMessage = async (socket, outgoingMsg) => {
-  socket.to('123').emit('receiveMsg', outgoingMsg);
-};
-
-// User disconnects -----------------------------------------------------
-const userDisconnect = socketId => {
-  console.log(`${socketId} has left the site.`);
-  db.user.clearSocketId(socketId);
 };
 
 /* SOCKET HANLDER */
@@ -88,17 +25,17 @@ const handleSocket = server => {
 
     socket.on('login', async ({ userId, chatId }) => {
       console.log('login ping');
-      registerSocketId(socket, userId);
+      onLogin.registerSocketId(socket, userId);
+      onLogin.joinChatrooms(socket, userId);
+      onLogin.notifyFriends(socket, userId);
       //  addChatroom(socket, userId, chatId);
-      joinChatrooms(socket, userId);
-      notifyFriends(socket, userId);
     });
 
     socket.on('sendMsg', async msgObject => {
       // ⚠️ Check if first message, if first message, create chatroom
-      const translations = await translateMessage(msgObject);
+      const translations = await onSend.translateMessage(msgObject);
       const outgoingMsg = { ...msgObject, translations, timestamp: Date.now() };
-      sendMessage(socket, outgoingMsg);
+      onSend.sendMessage(socket, outgoingMsg);
       db.message.createMessage(outgoingMsg);
     });
 
@@ -146,7 +83,8 @@ const handleSocket = server => {
   });
 
   io.on('disconnect', socket => {
-    userDisconnect(socket.id);
+    console.log(`${socket.id} has left the site.`);
+    db.user.clearSocketId(socket.id);
   });
 };
 
