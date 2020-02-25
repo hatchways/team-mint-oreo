f7bd971f40f47c556ccd8a4338ff8b168558bfdb
Rocket-Fish <rocket.fish@mail.com>
2020-02-25 11:13:37 -0500

    gradiant and chat bubble resize completed

--- client/src/components/chat-message/chat-message.styles.jsx
+++ client/src/components/chat-message/chat-message.styles.jsx
@@ -2,12 +2,12 @@ import { makeStyles } from '@material-ui/styles';
 
 export const useStyles = makeStyles(theme => ({
   chatPaper: {
-    backgroundColor: '#86B9FF',
-    borderRadius: 20,
+    background: 'linear-gradient(90deg, rgba(64,144,255,1) 0%, rgba(124,179,255,1) 100%)',
+    borderRadius: '5px 20px 20px 20px',
   },
   senderPaper: {
     backgroundColor: '#dfdfdf',
     borderRadius: 20,
-    marginLeft: 50,
+    marginLeft: '20px 20px 5px 20px',
   },
 }));
